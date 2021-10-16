cf53bd342e29211eeb78e2b979fb4d333f0a6f1c
MiguelGil1 <miguel.gil1@udea.edu.co>
2021-10-16 15:48:00 -0500

    Se avanza en el sistema de usuarios

--- Codigo_Fuente/Proyecto_Final/idcatc.cpp
+++ Codigo_Fuente/Proyecto_Final/idcatc.cpp
@@ -40,27 +40,48 @@ void IDCATC::closeProfileInterfaz(){
 }
 
 void IDCATC::on_pB_Login_clicked(){
+    //Se resetea la barra de progreso
     ui->Label_Status->clear();
+
     //Se abre el archivo de usuarios
     ifstream usersFile;
     usersFile.open(PATH_TO_USERS);
     ui->progressBar->setValue(10);
 
-    //Variables temporales
+    //Variables temporales para recorrer usuarios en la base de datos de usuarios
     string Username ,Password, Level, Lives, ScoreFirstLevel, ScoreSecondLevel, ScoreThirdLevel;
-    bool FoundUser = false, FoundPassword = false;
+
+    /*
+     * Banderas booleanas:
+     -> FoundUser = Almacena true si se encontro el usuario en el archivo de texto
+        De lo contrario, almacena false.
+     -> FoundPassword = Almacena true si las contraseñnas coinciden. De lo contrario,
+        Almacena false.
+    */
+    static bool FoundUser = false, FoundPassword = false;
 
     while(!usersFile.eof()){
         usersFile >> Username >> Password >> Level >> Lives >> ScoreFirstLevel >> ScoreSecondLevel >> ScoreThirdLevel;
+
+        /*cout << "Nombre: " << Username << endl;
+        cout << "Password: " << Password << endl;
+        cout << "Level: " << Level << endl;
+        cout << "Lives: " << Lives << endl;
+        cout << "Socre 1: " << ScoreFirstLevel << endl;
+        cout << "Score 2: " << ScoreSecondLevel << endl;
+        cout << "Score 3: " << ScoreThirdLevel << endl << endl;*/
+
         ui->progressBar->setValue(15);
+
         if(ui->Text_Username->text().toStdString() == Username){
             //Se encontro el usuario que se ingreso el QLineEdit
-
             FoundUser = true;
+
             ui->progressBar->setValue(70);
+
             //Se procede a verificar que las contraseñas coincidan
             if(ui->Text_Password->text().toStdString() == Password){
-
+                //**Asignacion de atributos de usuarios**//
                 mUsername         =              Username;
                 mPassword         =              Password;
                 mLevel            =           stoi(Level);
@@ -71,7 +92,6 @@ void IDCATC::on_pB_Login_clicked(){
 
                 ui->progressBar->setValue(100);
                 FoundPassword = true;
-                connect(ui->pB_Login, &QPushButton::clicked, this, &IDCATC::openProfileInterfaz);
                 break;
             }
         }
@@ -80,13 +100,18 @@ void IDCATC::on_pB_Login_clicked(){
     ui->Text_Username->clear();
     ui->Text_Password->clear();
 
-    if(FoundUser == false){
+    if(FoundUser == true && FoundPassword == true){
+            cout << "Datos correctos :)" << endl;
+            FoundPassword = false;
+            FoundUser = false;
+            openProfileInterfaz();
+    }else if(FoundUser == false){
         ui->Label_Status->setText("User not found.");
-        //ui->progressBar->setValue(0);
-    }else if(FoundUser == true || FoundPassword == false){
+        FoundPassword = false;
+        FoundUser = false;
+    }else if(FoundUser == true && FoundPassword == false){
         ui->Label_Status->setText("Passwords do not match.");
-        //ui->progressBar->setValue(0);
+        FoundPassword = false;
+        FoundUser = false;
     }
-    FoundUser = false;
-    FoundPassword = false;
 }
